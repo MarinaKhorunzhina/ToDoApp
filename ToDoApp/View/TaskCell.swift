8aecf4e2246252e0356e1ddde4b062b925416436
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-06-13 21:02:04 +0300

    MoskTableView, MostTaskCell, Done, Undone

---
+++ ToDoApp/View/TaskCell.swift
@@ -0,0 +1,14 @@
+//
+//  TaskCell.swift
+//  ToDoApp
+//
+//  Created by Marina on 10.06.22.
+//
+
+import UIKit
+
+
+    class TaskCell: UITableViewCell {
+        func configure(withTask task: Task) {}
+    }
+
