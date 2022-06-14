5918ceb1a4181c857520bf8f90dd56055c963b69
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-06-14 21:00:52 +0300

    DetailViewController,DetailViewTests

---
+++ ToDoApp/Controller/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  ToDoApp
+//
+//  Created by Marina on 14.06.22.
+//
+
+import UIKit
+import MapKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    
+    @IBOutlet var mapView: MKMapView!
+    
+    }
+    
+
+
--- ToDoApp/View/TaskCell.swift
+++ ToDoApp/View/TaskCell.swift
@@ -14,18 +14,26 @@ import UIKit
         @IBOutlet var dateLabel: UILabel!
         
         
-        func configure(withTask task: Task) {
-            self.titleLabel.text = task.title
-            
+        private var dateFormatter: DateFormatter {
             let df = DateFormatter()
             df.dateFormat = "dd.MM.yy"
-            if let date = task.date {
-                let dateString = df.string(from: date)
-                dateLabel.text = dateString
-            }
-            
-            self.locationLabel.text = task.location?.name
+            return df
+        }
         
+        func configure(withTask task: Task, done: Bool = false) {
+            
+            if done {
+                let attributedString = NSAttributedString(string: task.title, attributes: [NSAttributedString.Key.strikethroughStyle : NSUnderlineStyle.single.rawValue])
+                titleLabel.attributedText = attributedString
+                dateLabel = nil
+                locationLabel = nil
+            } else {
+                if let date = task.date {
+                    let dateString = dateFormatter.string(from: date)
+                    dateLabel.text = dateString
+                }
+                self.titleLabel.text = task.title
+                self.locationLabel.text = task.location?.name
+            }
         }
     }
-
