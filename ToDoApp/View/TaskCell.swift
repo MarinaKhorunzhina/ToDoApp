db178995f406e45e15ed188615d7e0dfa9c66472
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-06-14 19:38:44 +0300

    TaskCellTests

--- ToDoApp/View/TaskCell.swift
+++ ToDoApp/View/TaskCell.swift
@@ -9,6 +9,23 @@ import UIKit
 
 
     class TaskCell: UITableViewCell {
-        func configure(withTask task: Task) {}
+        @IBOutlet var titleLabel: UILabel!
+        @IBOutlet var locationLabel: UILabel!
+        @IBOutlet var dateLabel: UILabel!
+        
+        
+        func configure(withTask task: Task) {
+            self.titleLabel.text = task.title
+            
+            let df = DateFormatter()
+            df.dateFormat = "dd.MM.yy"
+            if let date = task.date {
+                let dateString = df.string(from: date)
+                dateLabel.text = dateString
+            }
+            
+            self.locationLabel.text = task.location?.name
+        
+        }
     }
 
