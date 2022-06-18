059379b6828ca36a7a9a5db9c52ec402bddc16ba
MarinaKhorunzhina <101120283+MarinaKhorunzhina@users.noreply.github.com>
2022-06-18 23:31:45 +0300

    Coordinate

--- ToDoApp/Controller/NewTaskViewController.swift
+++ ToDoApp/Controller/NewTaskViewController.swift
@@ -6,22 +6,41 @@
 //
 
 import UIKit
-
+import CoreLocation
 class NewTaskViewController: UIViewController {
 
     @IBOutlet var titleTextField: UITextField!
     @IBOutlet var locationTextField: UITextField!
     @IBOutlet var dateTextField: UITextField!
-    @IBOutlet var adressTextField: UITextField!
+    @IBOutlet var addressTextField: UITextField!
     @IBOutlet var descriptionTextField: UITextField!
     
     
     @IBOutlet var saveButton: UIButton!
-    
     @IBOutlet var cancelButton: UIButton!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    var taskManager: TaskManager!
+    var geocoder = CLGeocoder()
+    
+    var dateFormatter: DateFormatter {
+        let df = DateFormatter()
+        df.dateFormat = "dd.MM.yy"
+        return df
     }
+    
+    @IBAction func save () {
+        let titleString = titleTextField.text
+        let locationString = locationTextField.text
+        let date = dateFormatter.date(from: dateTextField.text!)
+        let descriptionString = descriptionTextField.text
+        let addressString = addressTextField.text
+        geocoder.geocodeAddressString(addressString!) { [unowned self] (placemarks, error) in
+            let placemark = placemarks?.first
+            let coordinate = placemark?.location?.coordinate
+            let location = Location(name: locationString!, coordinate: coordinate)
+            let task = Task(title: titleString!, description: descriptionString, date: date, location: location)
+            self.taskManager.add(task: task)
+        }
+    }
+    
 }
